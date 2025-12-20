145c3f98684bfc8f5ec8ff0810c7fd721f674a88
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-12-21 01:59:48 +0530

    added feature that after execution of order an unique id will be genrated

--- src/modules/orders/order.controller.ts
+++ src/modules/orders/order.controller.ts
@@ -1,16 +1,21 @@
 import { FastifyInstance } from "fastify";
-import { orderQueue } from "../queue/order.queue.js";
 import { v4 as uuid } from "uuid";
+import { orderQueue } from "../queue/order.queue";
+import { orderSchema } from "./order.schema";
 
 export async function orderRoutes(app: FastifyInstance) {
-  app.post("/api/orders/execute", async (req) => {
-    const orderId = uuid();
+  app.post(
+    "/api/orders/execute",
+    { schema: orderSchema },
+    async (req) => {
+      const orderId = uuid();
 
-    await orderQueue.add("execute", {
-      orderId,
-      payload: req.body
-    });
+      await orderQueue.add("execute", {
+        orderId,
+        payload: req.body
+      });
 
-    return { orderId };
-  });
+      return { orderId };
+    }
+  );
 }
