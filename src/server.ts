206b4105647effa042f2427f2b803af181227099
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-12-20 17:21:53 +0530

    added swager import

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import Fastify from "fastify";
-import websocket from "fastify-websocket";
-import { orderRoutes } from "./modules/orders/order.controller.js";
-import { orderWebsocket } from "./modules/orders/order.ws.js";
+import websocket from "@fastify/websocket";
+import { registerSwagger } from "./plugins/swagger";
+import { orderRoutes } from "./modules/orders/order.controller";
+import { orderWebsocket } from "./modules/orders/order.ws";
 
-const app = Fastify();
-app.register(websocket);
+const app = Fastify({ logger: true });
+
+await app.register(websocket);
+await registerSwagger(app);
 
 await orderRoutes(app);
 orderWebsocket(app);
 
 app.listen({ port: 3000 }, () => {
-  console.log("🚀 API running on http://localhost:3000");
+  console.log("🚀 Server running at http://localhost:3000");
 });
