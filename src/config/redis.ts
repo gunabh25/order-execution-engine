22d909cd48189b48133bcc37bc75096c3136f42a
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-12-21 11:59:18 +0530

    fix: Redis Config

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,3 +1,14 @@
 import Redis from "ioredis";
 
-export const redisConnection = new Redis();
+export const redisConnection = new Redis({
+  maxRetriesPerRequest: null, // REQUIRED by BullMQ
+  enableReadyCheck: false
+});
+
+redisConnection.on("connect", () => {
+  console.log("🟢 Redis connected");
+});
+
+redisConnection.on("error", (err) => {
+  console.error("🔴 Redis error:", err.message);
+});
