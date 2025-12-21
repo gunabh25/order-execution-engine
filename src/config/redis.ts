2a4ab0ed4d9a561eee3b130acf321363b1960ae1
Gunabh Sharan <gunabhsharan.25@gmail.com>
2025-12-21 14:52:04 +0530

    changes in retrying function

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,12 +1,14 @@
 import Redis from "ioredis";
 
 export const redisConnection = new Redis({
-  maxRetriesPerRequest: null, // REQUIRED by BullMQ
-  enableReadyCheck: false
-});
-
-redisConnection.on("connect", () => {
-  console.log("🟢 Redis connected");
+  host: "127.0.0.1",
+  port: 6379,
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
+  retryStrategy: (times) => {
+    if (times > 5) return null; // stop retrying
+    return 500;
+  }
 });
 
 redisConnection.on("error", (err) => {
